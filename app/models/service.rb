fe221dcf4c5c361e86cbd1cc5916e49169c33c08
addisaden <addis.aden@gmail.com>
2014-02-24 11:32:24 +0100

    make service short name upcase before save

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,4 +1,12 @@
 class Service < ActiveRecord::Base
+  before_save :make_short_upcase
+
   validates :title, uniqueness: true, presence: true
   validates :short, uniqueness: true, presence: true
+
+  private
+
+  def make_short_upcase
+    self.short = self.short.upcase
+  end
 end
